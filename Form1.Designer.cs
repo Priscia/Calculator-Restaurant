c491af46dfcee6d8c1f28b07c8a19ae9236e0b1a
priscia <prisciakibibi@gmail.com>
2017-07-16 09:19:11 -0500

    Addition Uploads
    C# Homework Project 1

---
+++ Form1.Designer.cs
@@ -0,0 +1,241 @@
+﻿namespace Salman_proj
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.comboBox4 = new System.Windows.Forms.ComboBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Soda",
+            "Tea",
+            "Coffee",
+            "Mineral Water",
+            "Juice",
+            "Milk"});
+            this.comboBox1.Location = new System.Drawing.Point(12, 58);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(184, 21);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.Text = "Beverages";
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "Buffalo Wings",
+            "Buffalo Finger",
+            "Potato Skins",
+            "Nachos",
+            "Mushroom Caps",
+            "Shrimp Cocktail",
+            "Chips and Salsa"});
+            this.comboBox2.Location = new System.Drawing.Point(202, 58);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(174, 21);
+            this.comboBox2.TabIndex = 1;
+            this.comboBox2.Text = "Appetizers";
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Items.AddRange(new object[] {
+            "Chicken Alfredo",
+            "Chicken Picatta",
+            "Turkey Club",
+            "Lobster Pie",
+            "Prime Rib",
+            "Shrimp Scampi",
+            "Turkey Dinner",
+            "Stuffed Chicken",
+            "Seafood Alfredo"});
+            this.comboBox3.Location = new System.Drawing.Point(382, 58);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(171, 21);
+            this.comboBox3.TabIndex = 2;
+            this.comboBox3.Text = "Main Couse";
+            this.comboBox3.SelectedIndexChanged += new System.EventHandler(this.comboBox3_SelectedIndexChanged);
+            // 
+            // comboBox4
+            // 
+            this.comboBox4.FormattingEnabled = true;
+            this.comboBox4.Items.AddRange(new object[] {
+            "Apple Pie",
+            "Sundae",
+            "Carrot Cake",
+            "Mud Pie",
+            "Apple Crisp"});
+            this.comboBox4.Location = new System.Drawing.Point(559, 58);
+            this.comboBox4.Name = "comboBox4";
+            this.comboBox4.Size = new System.Drawing.Size(170, 21);
+            this.comboBox4.TabIndex = 3;
+            this.comboBox4.Text = "Desserts";
+            this.comboBox4.SelectedIndexChanged += new System.EventHandler(this.comboBox4_SelectedIndexChanged);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(202, 132);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(351, 147);
+            this.listBox1.TabIndex = 4;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(614, 376);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Clear Bill";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox3);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(202, 302);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(351, 100);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Bill";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(169, 77);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(176, 20);
+            this.textBox3.TabIndex = 5;
+            this.textBox3.Text = "0";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(169, 45);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(176, 20);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.Text = "0";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(169, 23);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(176, 20);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Text = "0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(31, 84);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Total   :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(31, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Tax   :";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(31, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Sub Total   :";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(747, 430);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.comboBox4);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Restaurant Bill Calculator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.ComboBox comboBox4;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Salman_proj
+{
+
+
+    public partial class Form1 : Form
+    {
+        double tax=0;
+        double total = 0;
+        double[] Beverages = new double[6];
+        double[] Appetizers = new double[7];
+        double[] MainCourse = new double[9];
+        double[] Dessert = new double[5];
+        public Form1()
+        {
+            InitializeComponent();
+            Beverages[0]= 1.95;
+            Beverages[1]= 1.50;
+            Beverages[2] = 1.25;
+            Beverages[3]= 2.95;
+            Beverages[4]= 2.50;
+            Beverages[5] = 1.50;
+            Appetizers[0] = 5.95;
+            Appetizers[1] = 6.95;
+            Appetizers[2] = 8.95;
+            Appetizers[3] = 8.95;
+            Appetizers[4] = 10.95;
+            Appetizers[5] = 12.95;
+            Appetizers[6] = 6.95;
+            MainCourse[0] = 13.95;
+            MainCourse[1] = 13.95;
+            MainCourse[2] = 11.95;
+            MainCourse[3] = 19.95;
+            MainCourse[4] = 20.95;
+            MainCourse[5] = 18.95;
+            MainCourse[6] = 13.95;
+            MainCourse[7] = 14.95;
+            MainCourse[8] = 15.95;
+            Dessert[0] = 3.95;
+            Dessert[1] = 5.95;
+            Dessert[2] = 4.95;
+            Dessert[3] = 5.95;
+            Dessert[4] = 5.95;
+            listBox1.Text = "";
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            double price;
+            int index = comboBox1.SelectedIndex;
+            listBox1.Items.Add(comboBox1.SelectedItem+"                     "+(Beverages[index]).ToString());
+            if (textBox1.Text == "")
+            {
+                textBox1.Text = Beverages[index].ToString();
+            }
+            else
+            {
+                price = double.Parse(textBox1.Text);
+                price = price + Beverages[index];
+                textBox1.Text = price.ToString();
+            }
+            
+            tax = double.Parse(textBox1.Text)*(8.25/100);
+            textBox2.Text = tax.ToString();
+            total = double.Parse(textBox1.Text) + double.Parse(textBox2.Text);
+            textBox3.Text = total.ToString();
+            
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            double price;
+            int index = comboBox2.SelectedIndex;
+            listBox1.Items.Add(comboBox2.SelectedItem + "                     " + (Appetizers[index]).ToString());
+            if (textBox1.Text == "")
+            {
+                textBox1.Text = Beverages[index].ToString();
+            }
+            else
+            {
+                price = double.Parse(textBox1.Text);
+                price = price + Appetizers[index];
+                textBox1.Text = price.ToString();
+            }
+            tax = double.Parse(textBox1.Text) * (8.25 / 100);
+            textBox2.Text = tax.ToString();
+            total = double.Parse(textBox1.Text) + double.Parse(textBox2.Text);
+            textBox3.Text = total.ToString();
+        }
+
+        private void comboBox3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            double price;
+            int index = comboBox3.SelectedIndex;
+            listBox1.Items.Add(comboBox3.SelectedItem + "                     " + (MainCourse[index]).ToString());
+            if (textBox1.Text == "")
+            {
+                textBox1.Text = Beverages[index].ToString();
+            }
+            else
+            {
+                price = double.Parse(textBox1.Text);
+                price = price + MainCourse[index];
+                textBox1.Text = price.ToString();
+            }
+            tax = double.Parse(textBox1.Text) * (8.25 / 100);
+            textBox2.Text = tax.ToString();
+            total = double.Parse(textBox1.Text) + double.Parse(textBox2.Text);
+            textBox3.Text = total.ToString();
+        }
+
+        private void comboBox4_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            double price;
+            int index = comboBox4.SelectedIndex;
+            listBox1.Items.Add(comboBox4.SelectedItem + "                     " + (Dessert[index]).ToString());
+            if (textBox1.Text == "")
+            {
+                textBox1.Text = Beverages[index].ToString();
+            }
+            else
+            {
+                price = double.Parse(textBox1.Text);
+                price = price + Dessert[index];
+                textBox1.Text = price.ToString();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            listBox1.Items.Clear();
+            textBox1.Text = "0";// = "";
+            textBox2.Text = "0";//.Clear();//.Text = "";
+            textBox3.Text = "0";//.Clear();//.Text = "";
+            total = 0;
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
